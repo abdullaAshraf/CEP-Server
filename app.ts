5c809b03f5f90a01aefaa0c856d2228e0f159031
abdulla-ashraf <abdulla@funnelll.com>
2022-02-06 03:22:05 -0500

    body logger

--- app.ts
+++ app.ts
@@ -10,6 +10,8 @@ import clusterRouter from './routes/cluster'
 import Scheduler from './services/scheduler';
 import ClusterManager from './services/clusterManager';
 
+var winston = require('winston'), expressWinston = require('express-winston');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -25,6 +27,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+expressWinston.requestWhitelist.push('body')
+expressWinston.responseWhitelist.push('body')
+app.use(expressWinston.logger({
+      transports: [
+        new winston.transports.Console({
+          json: true,
+          colorize: true
+        })
+      ],
+      meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+      msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+      expressFormat: true, // Use the default Express/morgan request formatting, with the same colors. Enabling this will override any msg and colorStatus if true. Will only output colors on transports with colorize set to true
+      colorStatus: true, // Color the status code, using the Express/morgan color palette (default green, 3XX cyan, 4XX yellow, 5XX red). Will not be recognized if expressFormat is true
+    }));
 
 app.use('/', indexRouter);
 app.use('/service', serviceRouter);
